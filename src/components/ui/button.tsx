c1235d74ff1d33a524756031245fb2bf4a5ff401
leoka2000 <reusoliveiraleo@gmail.com>
2025-08-19 16:53:31 +0200

    style fix

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,12 +5,12 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all dark:text-neutral-200 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
         default:
-          "bg-primary border  border-emerald-500 hover:border-emerald-500 dark:border-emerald-600 dark:hover:border-emerald-300 dark:hover:bg-emerald-300 text-primary-foreground dark:hover:text-gray-800 shadow-xs  hover:bg-green-300 cursor-pointer",
+          "bg-primary border  border-emerald-500 hover:border-emerald-700 hover:shadow-lg dark:bg-[#00623A] dark:hover:bg-[#2C7151] dark:border-[#097246] dark:border-emerald-600 dark:hover:border-emerald-500  text-neutral-900 shadow-xs   cursor-pointer",
         destructive:
           " cursor-pointer bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
